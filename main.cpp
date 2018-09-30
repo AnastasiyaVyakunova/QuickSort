faea7484ee2b5b3e220a1d89430e6e423f8557b1
AnastasiyaVyakunova <anastasiya.vyakunova@yandex.ru>
2018-09-30 13:12:18 +0300

    Second

--- main.cpp
+++ main.cpp
@@ -9,25 +9,26 @@ vector<int> quickSort(vector<int> a)
 	vector<int>l;
 	vector<int>h;
 	int n = a.size();
-	
 
-			for (int i = 0; i < n; i++)
+	int max = a[0];
+			for (int i = 1; i < n; i++)
 			{
-				int max = a[0];
 				if (!a.empty())
 				{
-					if (max > a[i]) l.push_back(a[i]);
+					if (max >= a[i]) l.push_back(a[i]);
 					else h.push_back(a[i]);
 				}
 				else  return a;
 			}
-			if ((l.size() > 1) && (h.size() > 1))
+
+			if ((l.size() >1) && (h.size() >1))
 			{
 				l = quickSort(l);
 				h = quickSort(h);
 			}
-			if ((l.size() == 0) && (h.size() > 1)) h = quickSort(h);
-			if ((l.size() > 1) && (h.size() == 0)) l = quickSort(l);
+			if ((l.size() == 0) && (h .size() >1)) h = quickSort(h);
+			if ((l.size() >1) && (h.size() == 0)) l = quickSort(l);
+			l.push_back(max);
 			l.insert(l.end(), h.begin(), h.end());
 			return l;
 		}
